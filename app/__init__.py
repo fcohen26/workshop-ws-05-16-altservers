cf41f581819a5222f41c5c1c489ac4a1c71925ee
Frances Cohen <fcohen26@gmail.com>
2019-05-16 15:16:51 -0400

    final

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -23,8 +24,15 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
+    @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
+    from . import db
+    db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     return app
